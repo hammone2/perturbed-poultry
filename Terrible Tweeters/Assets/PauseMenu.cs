7ecefc94a1105681688c864ae83dc88473d3e640
hammone2 <hammone2@my.erau.edu>
2024-04-14 23:42:23 -0700

    trying to get cutscene to work

--- Terrible Tweeters/Assets/PauseMenu.cs
+++ Terrible Tweeters/Assets/PauseMenu.cs
@@ -47,6 +47,6 @@ public class PauseMenu : MonoBehaviour
     public void NewGame()
     {
         Resume();
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("MainMenu");
     }
 }
